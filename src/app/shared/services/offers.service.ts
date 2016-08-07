68610cebea67916eb851a15ef983f89e2b76518a
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2016-08-07 19:41:29 +0200

    Add authorized request

--- src/app/shared/services/offers.service.ts
+++ src/app/shared/services/offers.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http, RequestOptions } from '@angular/http';
+import { AuthHttp } from 'angular2-jwt';
 import { IComment } from '../interfaces/comment.interface';
 import { IOffer } from '../interfaces/offer.interface';
 
 @Injectable()
 export class OffersService {
   public currentOffer: IOffer;
-  
-  constructor(private http: Http) {}
+
+  constructor(
+    private http: Http,
+    private authHttp: AuthHttp
+  ) {}
 
   getOfferForUrl(url: string) {
     const body = JSON.stringify({ url });
@@ -33,6 +37,7 @@ export class OffersService {
     const headers = new Headers({ 'Content-Type': 'application/json' });
     const options = new RequestOptions({ headers: headers });
 
+    // return this.authHttp
     return this.http
       .post(`${API_URL}/offers/${offerId}/comments`, body, options)
       .map(request => request.json());
--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { provideRouter } from '@angular/router';
 import { provideForms, disableDeprecatedForms } from '@angular/forms';
 import { HTTP_PROVIDERS } from '@angular/http';
+import { AUTH_PROVIDERS } from 'angular2-jwt';
+
 
 import { AppComponent } from './app/app.component';
 import { AppRoutes } from './app/app.routes';
@@ -20,5 +22,6 @@ bootstrap(AppComponent, [
   disableDeprecatedForms(),
   provideForms(),
   HTTP_PROVIDERS,
+  AUTH_PROVIDERS,
   OffersService
 ]);
